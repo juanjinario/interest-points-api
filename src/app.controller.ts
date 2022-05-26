6acb14a56c03bfb334081ecd2a65123dbf1be3c9
juanjose_calderon_kairosds <juanjose.calderon@kairosds.com>
2022-05-26 05:10:17 +0200

    feat!: add get places by coords from Nearby Google Maps

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,6 +12,6 @@ export class AppController {
 
   @Get('place')
   getAllPlaces() {
-    return 'eat my dick';
+    return this.appService.findAllPlaces({});
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,32 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+import { limitCoords } from './data';
+import Utils from './helpers/utils';
 
 @Injectable()
 export class AppService {
+  gmapKey = '%20AIzaSyCjJLtjmrhwSmSCTLPH-Wi4nGsY_Yx8Ogw' as const;
+  baseUrl =
+    'https://maps.googleapis.com/maps/api/place/nearbysearch/json' as const;
+  radius = 1110 as const; // Distance between 0.01 latitude/longitude
+
   constructor(private httpService: HttpService) {}
 
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello to interest points api!';
   }
 
-  findAllPlaces() {
-    return 'webo';
+  async findAllPlaces({ type = 'bus_station' }): Promise<any> {
+    const key = this.gmapKey;
+    const location = Utils.coordsToString(limitCoords['top-left']);
+    return await this.getPlacesByCoord({ key, location, radius: this.radius });
+  }
+
+  async getPlacesByCoord({ key, location, radius, type = 'bus_station' }) {
+    const url = `${this.baseUrl}?key=${key}&location=${location}&radius=${radius}&type=${type}`;
+    const { data } = await firstValueFrom(this.httpService.get(url));
+    console.log(data);
+    return data;
   }
 }
