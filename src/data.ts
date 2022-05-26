b32e664edfd9d1fa495075c37a3f26bee9c3ca63
juanjose_calderon_kairosds <juanjose.calderon@kairosds.com>
2022-05-26 05:09:36 +0200

    feat: add data and utils required for get interest points

---
+++ src/data.ts
@@ -0,0 +1,22 @@
+// punto 1) 39.83 , -1.00  (esquina superior izquierda)
+// punto 2) 39.83 , -0.11  (esquina superior derecha)
+// punto 3) 39,14 , -1.00  (esquina inferior izquierda)
+// punto 4) 39.14 , -0.11  (esquina inferior derecha
+export const limitCoords = {
+  'top-left': {
+    lat: 39.83,
+    long: -1.00,
+  },
+  'top-right': {
+    lat: 39.83,
+    long: -0.11,
+  },
+  'bottom-left': {
+    lat: 39.14,
+    long: -1.00,
+  },
+  'bottom-right': {
+    lat: 39.14,
+    long: -0.11,
+  },
+};
