1e506d7a827df7c8653c1291d117666d37d3dcf9
juanjose_calderon_kairosds <juanjose.calderon@kairosds.com>
2022-05-26 04:31:16 +0200

    feat: add getPlace structure controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('place')
+  getAllPlaces() {
+    return 'eat my dick';
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  constructor(private httpService: HttpService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  findAllPlaces() {
+    return 'webo';
   }
 }
